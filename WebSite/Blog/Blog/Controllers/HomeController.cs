bea67c550ce69aa45acd6c05b125e686ac33123c
iliya87 <blunt8706@gmail.com>
2016-08-28 21:58:31 +0300

    Project - Blog
    Start new Project Asp.net. Blog with CRUD operation.

---
+++ WebSite/Blog/Blog/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var post = db.Posts.OrderByDescending(p => p.Date).Take(3);
+            return View(post.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebSite/Blog/Blog/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Comment
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public Post Post { get; set; }
+    }
+}
